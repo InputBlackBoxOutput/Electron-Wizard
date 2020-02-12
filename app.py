d254e90b23c45c072621e56de2921c3527e97193
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2020-02-12 18:04:51 +0530

    Updated webapp wireframe

--- app.py
+++ app.py
@@ -7,40 +7,93 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '4d12bc03c6bdcc7a945a55d1316490fa'
 
-#Home Page
+#------------------------------------------------------------------------------------------
+# Basic routes
+
 @app.route("/")
 @app.route("/home")
 def home():
 	return render_template("home.html", title="Home") 
 
-#About Page
+
 @app.route("/about")
 def about():
 	return render_template("about.html", title="About")
 
+# Error Page
+@app.route('/error')
+def error():
+    return render_template('error.html',title='Error Occured')    
 
-@app.route("/ex", methods=['GET', 'POST'])
-def ex():
-    form = forms.R_Comb()
+# 
+@app.route('/fun')
+def fun():
+    return redirect('https://dino-chrome.com') 
+
+#------------------------------------------------------------------------------------------
+# Calculator routes
+
+@app.route("/Ohms_Law", methods=['GET', 'POST'])
+def Ohms_Law():
+    form = forms.Ohm()
     
     if form.validate_on_submit():
-        result = int(form.Rone.data) + int(form.Rtwo.data)
-        return render_template('ex.html', form=form, result=result)
+        result = form.R.data
+        return render_template('Ohms_Law.html', form=form, result=result)
     else:
-        return render_template('ex.html', form=form)
+        return render_template('Ohms_Law.html', form=form)
 
     if form.error:
         print('Error Occured')
         return redirect(url_for('error'))
 
-@app.route('/error')
-def error():
-    return render_template('error.html',title='Error Occured')    
 
-@app.route('/fun')
-def fun():
-    return redirect('https://dino-chrome.com') 
+@app.route("/Resistor_Series_Parallel", methods=['GET', 'POST'])
+def Resistor_Series_Parallel():
+    form = forms.R_Comb()
+    
+    if form.validate_on_submit():
+        result = form.Rone.data + form.Rtwo.data
+        return render_template('Resistor_Series_Parallel.html', form=form, result=result)
+    else:
+        return render_template('Resistor_Series_Parallel.html', form=form)
 
+    if form.error:
+        print('Error Occured')
+        return redirect(url_for('error'))
+
+@app.route("/Resistor_Colour_Code", methods=['GET', 'POST'])
+def Resistor_Colour_Code():
+    return render_template('Resistor_Colour_Code.html')
+    # form = forms.R()
+    
+    # if form.validate_on_submit():
+    #     result = 'In development'
+    #     return render_template('Resistor_Colour_Code.html', form=form, result=result)
+    # else:
+    #     return render_template('Resistor_Colour_Code.html', form=form)
+
+    # if form.error:
+    #     print('Error Occured')
+    #     return redirect(url_for('error'))
+
+
+@app.route("/SMD_Code", methods=['GET', 'POST'])
+def SMD_Code():
+    return render_template('SMD_Code.html')
+    # form = forms.SMD()
+    
+    # if form.validate_on_submit():
+    #     result = 'In development'
+    #     return render_template('SMD_Code.html', form=form, result=result)
+    # else:
+    #     return render_template('SMD_Code.html', form=form)
+
+    # if form.error:
+    #     print('Error Occured')
+    #     return redirect(url_for('error'))
+
+#------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 	app.run(debug =True)
  
--- forms.py
+++ forms.py
@@ -1,39 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import DecimalField, RadioField, SelectField
 from wtforms.validators import DataRequired
 
 # 1) Ohms law
 class Ohm(FlaskForm):
-    R = StringField('R', validators=[])
-    V = StringField('V', validators=[])
-    I = StringField('I', validators=[])
+    R = DecimalField('R', default=-1)
+    V = DecimalField('V', default=-1)
+    I = DecimalField('I', default=-1)
 
 # 2) Through hole resistor value
 class R(FlaskForm):
-	color1 = StringField('Band 1', validators=[DataRequired()])
+	n_bands = RadioField('Number of bands on resistor', validators=[DataRequired], choices=['3','4','5'])
+	color1 = SelectField('Band 1', validators=[DataRequired()])
+	color2 = SelectField('Band 2', validators=[DataRequired()])
+	color3 = SelectField('Band 3', validators=[DataRequired()])
+	color4 = SelectField('Band 4', default=None)
+	color5 = SelectField('Band 5', default=None)
 
 # 3) SMD Resistor value
 # 4) SMD Capacitor value
 # 5) SMD Inductor value
 class SMD(FlaskForm):
-	M = StringField('Marking', validators=[DataRequired()])
+	compnt = RadioField('Select component', validators=[DataRequired], choices=['Resistor','Capacitor','Inductor'])
+	code = DecimalField('Code', validators=[DataRequired()])
 
 # 6) Voltage divider
 class V_Div(FlaskForm):
-	V = StringField('V', validators=[DataRequired()])
-	R1 = StringField('R1', validators=[DataRequired()])
-	R2 = StringField('R2', validators=[DataRequired()])
+	V = DecimalField('V', validators=[DataRequired()])
+	R1 = DecimalField('R1', validators=[DataRequired()])
+	R2 = DecimalField('R2', validators=[DataRequired()])
 
 # 7) Resistors in series/parallel
 class R_Comb(FlaskForm):
-    Rone = StringField('R1', validators=[DataRequired()])
-    Rtwo = StringField('R2', validators=[DataRequired()])
+    Rone = DecimalField('R1', validators=[DataRequired()])
+    Rtwo = DecimalField('R2', validators=[DataRequired()])
 
 # 8) LED resistor
 class LED_R(FlaskForm):
-	Vdrop = StringField('Vdrop', validators=[DataRequired()])
-	V = StringField('V', validators=[DataRequired()])
-	I = StringField('I', validators=[DataRequired()])
+	Vdrop = DecimalField('Vdrop', validators=[DataRequired()])
+	V = DecimalField('V', validators=[DataRequired()])
+	I = DecimalField('I', validators=[DataRequired()])
 
 
 
