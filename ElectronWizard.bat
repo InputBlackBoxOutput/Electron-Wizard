60b7122e94e9ebbea4a1f0d95ff3253bb72635cb
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2020-02-15 09:29:24 +0530

    Corrected ElectronWizard.bat
    &  was causing cmd to misbehave :-)

--- ElectronWizard.bat
+++ ElectronWizard.bat
@@ -4,7 +4,7 @@ ECHO Developed by Rutuparn Pawar (InputBlackBoxOutput)
 
 ECHO If nothing happened, do the following:
 ECHO 1) Open your favorite browser  
-ECHO 2) Copy & paste one of the following URLs
+ECHO 2) Copy and paste one of the following URLs
 ECHO https://localhost:5000/ or https://127.0.0.1:5000/
 
 SET FLASK_APP=app.py
